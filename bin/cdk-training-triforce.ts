c2a2cf46ca10eaaedef227331e53016b267713fb
jlui17g <justin.lui@wegalvanize.com>
2022-04-22 13:17:19 -0700

    init cdk app

--- bin/cdk-training-triforce.ts
+++ bin/cdk-training-triforce.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { PictureRaterStack } from '../lib/picture-rater-stack';
+import { CdkTrainingTriforceStack } from '../lib/cdk-training-triforce-stack';
 
 const app = new cdk.App();
-new PictureRaterStack(app, 'PictureRaterStack', {
+new CdkTrainingTriforceStack(app, 'CdkTrainingTriforceStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- test/cdk-training-triforce.test.ts
+++ test/cdk-training-triforce.test.ts
@@ -1,13 +1,13 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as PictureRater from '../lib/picture-rater-stack';
+// import * as CdkTrainingTriforce from '../lib/cdk-training-triforce-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/picture-rater-stack.ts
+// example resource in lib/cdk-training-triforce-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
-//   const stack = new PictureRater.PictureRaterStack(app, 'MyTestStack');
+//   const stack = new CdkTrainingTriforce.CdkTrainingTriforceStack(app, 'MyTestStack');
 //     // THEN
 //   const template = Template.fromStack(stack);
 
