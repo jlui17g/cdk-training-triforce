d48737a85b61b6d387ce96396c792a297b500a15
jlui17g <justin.lui@wegalvanize.com>
2022-04-22 13:14:26 -0700

    init picture-rater app

---
+++ picture-rater/test/picture-rater.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PictureRater from '../lib/picture-rater-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/picture-rater-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PictureRater.PictureRaterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
