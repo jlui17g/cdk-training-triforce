d81a466ed6ef7a4f0132f88963f36d696162c950
jlui17g <justin.lui@wegalvanize.com>
2022-04-22 17:11:22 -0700

    endpoint with GET returns presigneds3url

--- lib/picture-uploader-service.ts
+++ lib/picture-uploader-service.ts
@@ -2,21 +2,47 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class PictureUploaderService extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
+    const pictures = new s3.Bucket(this, "pictures", {
+      cors: [
+        {
+          allowedMethods: [
+            s3.HttpMethods.GET,
+            s3.HttpMethods.POST,
+            s3.HttpMethods.PUT,
+          ],
+          allowedOrigins: ['*'],
+          allowedHeaders: ['*']
+        }
+      ]
+    });
+
     const handler = new lambda.Function(this, "PictureUploaderHandler", {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset("src/picture-uploader"),
       handler: "upload.main",
-      environment: {}
-    })
+      environment: {BUCKET_NAME: pictures.bucketName}
+    });
 
     const api = new apigateway.RestApi(this, "PictureUploaderAPI", {
       restApiName: "Picture Uploader Service",
-      description: "Upload pictures to an S3 bucket"
+      description: "Upload pictures to an S3 bucket",
+      defaultCorsPreflightOptions: {
+        allowHeaders: ['Content-Type', 'X-Amz-Date', 'Authorization', 'X-Api-Key'],
+        allowMethods: [
+          'GET',
+          'POST',
+          'PUT',
+          'DELETE'
+        ],
+        allowCredentials: false,
+        allowOrigins: ["*"],
+      },
     })
 
     const getPictureUploaderIntegration = new apigateway.LambdaIntegration(handler, {
--- src/picture-uploader/upload.ts
+++ src/picture-uploader/upload.ts
@@ -1,22 +1,56 @@
 const AWS = require('aws-sdk');
+import { S3 } from 'aws-sdk';
+
+const bucketName = process.env.BUCKET_NAME;
+
+type GetPresignedPostUrlParams = {
+  fileType: string;
+  fileName: string;
+};
+
+const createPresignedPost = ({ fileType, fileName }: GetPresignedPostUrlParams): Promise<S3.PresignedPost> => {
+  const params = {
+    Bucket: bucketName,
+    Fields: {key: fileName, acl: 'public-write'},
+    Conditions: [
+      // content length restrictions: 0-1MB]
+      ['content-length-range', 0, 1000000],
+      // specify content-type to be more generic- images only
+      // ['starts-with', '$Content-Type', 'image/'],
+      ['eq', '$Content-Type', fileType],
+    ],
+    // number of seconds for which the presigned policy should be valid
+    Expires: 15,
+  };
+
+  const s3 = new S3();
+  return s3.createPresignedPost(params) as unknown as Promise<S3.PresignedPost>;
+}
 
 exports.main = async (event, context) => {
-  // const method = event.httpMethod;
+  const method = event.httpMethod;
 
-  // if (method === "GET") {
-  console.log('hello world !');
-  const body = {message: 'hello world !'};
+  if (method !== "GET") {
+    return {
+      statusCode: 400,
+      headers: {},
+      body: JSON.stringify({message: `please do a GET request, ${method}`})
+    }
+  }
+
+  const fileType = event.queryStringParameters?.fileType;
+  const fileName = event.queryStringParameters?.fileName;
+
+  if (!fileType) return {
+    statusCode: 400,
+    headers: {},
+    body: JSON.stringify({message: 'please include file type'})
+  }
+
+  const presignedPost = await createPresignedPost({ fileType: fileType, fileName: fileName });
   return {
     statusCode: 200,
     headers: {},
-    body: JSON.stringify(body)
+    body: JSON.stringify({ data: presignedPost })
   }
 }
-
-  // const body = {message: 'not a get request'};
-  // return {
-  //   statusCode: 200,
-  //   headers: {},
-  //   body: JSON.stringify(body)
-  // }
-// }
